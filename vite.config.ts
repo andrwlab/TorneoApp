6650adbb9e99da1b59b97e6f16a1f2233e543803
andrwlab <andrewhallsm@gmail.com>
2025-05-05 22:39:58 -0500

    viteconfig2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// if you're using TS:
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // tell the plugin to treat `.js` as JSX
+      include: ['**/*.jsx', '**/*.tsx', '**/*.js'],
+    }),
+  ],
 })
