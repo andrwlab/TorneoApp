a2c98175d603926c7f29375b94b2de754f52fd1e
andrwlab <andrewhallsm@gmail.com>
2025-05-06 10:14:37 -0500

    erase all js files copies

--- src/pages/Admin.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Admin = function () {
-    return (<div className="p-6">
-        <h2 className="text-2xl font-bold mb-4">Panel de Administración</h2>
-        <p className="text-gray-700">Aquí podrás gestionar los partidos y estadísticas.</p>
-      </div>);
-};
-exports.default = Admin;
